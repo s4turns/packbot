d717176ad22d795caf8fee16c63e6253f344fd5f
s4turn's Run <53133058+s4turns@users.noreply.github.com>
2019-08-03 02:08:33 -0500

    Update packbot.js

--- packbot.js
+++ packbot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const client = require('./config.json');
+const client = require("./config.json");
 
 client.on('ready', () => {
   console.log('PackBot is up and running motherfucker!');
@@ -56,13 +56,16 @@ client.on('message', msg => {
 client.on("message", message => {
   if (message.author.bot) return;
   if (message.content.indexOf(config.prefix) !== 0) return;
- 
+
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
- 
+
   if(command === 'ping') {
   message.channel.send('Pong!');
   } else
+  if (command === 'blah') {
+  message.channel.send('Meh.');
+  }
   if (command === "asl") {
   let age = args[0];
   let sex = args[1];
