b3819bf288c30f7114fd3c520321cca39b48bbe5
s4turn's Run <53133058+s4turns@users.noreply.github.com>
2019-08-05 23:25:09 -0500

    updated

--- packbot.js
+++ packbot.js
@@ -73,16 +73,16 @@ client.on("message", message => {
   message.channel.send(`Hello ${message.author.username}, I see youre a ${age} year old ${sex} from ${location}. Wanna date?`);
   }
   else if (command === "bong") {
+    let user = args[0];
     if (!message.mentions.users.size) {
   	return message.reply('you need to tag a user dumbass!');
-  let user = args[0];
   message.channel.send(`passed the bong to ${user}. Lets get baked bitch!`);
     }
   }
   else if (command === "beer") {
+    let user = args[0];
     if (!message.mentions.users.size) {
   	return message.reply('you need to tag a user dumbass!');
-  let user = args[0];
   message.channel.send(`here ${user}! take this an ice cold beer! Its a Warsteiner!`);
     }
   }
