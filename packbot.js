684bc6302af23d3802ea6429dd6c4f949cb6ecbe
s4turn's Run <53133058+s4turns@users.noreply.github.com>
2019-08-09 21:31:35 -0500

    added token to const

--- packbot.js
+++ packbot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
+const { prefix, token } = require('./config.json');
 
 client.on('ready', () => {
   console.log('PackBot is up and running motherfucker!');
