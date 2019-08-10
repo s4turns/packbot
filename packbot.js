b531e8caf0282e16d6c4b63aa320c4c5ba45d5b8
s4turn's Run <53133058+s4turns@users.noreply.github.com>
2019-08-09 21:46:10 -0500

    updates

--- packbot.js
+++ packbot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
-const prefix = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 client.on('ready', () => {
   console.log('PackBot is up and running motherfucker!');
@@ -177,4 +176,4 @@ client.on('message', message => {
 	}
 });
 
-client.login(config.token);
+client.login(token);
