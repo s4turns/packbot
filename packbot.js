8705d5277aabe54a0313a2c54164665b0886267e
s4turn's Run <53133058+s4turns@users.noreply.github.com>
2019-08-09 21:47:53 -0500

    updated fu sup_g

--- packbot.js
+++ packbot.js
@@ -52,9 +52,9 @@ client.on('message', msg => {
 
 client.on("message", message => {
   if (message.author.bot) return;
-  if (message.content.indexOf(config.prefix) !== 0) return;
+  if (message.content.indexOf(prefix) !== 0) return;
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   if(command === 'bøtte') {
