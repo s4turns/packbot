54ac54b6f328c32e21ad5c542d165892ed88a7f7
s4turn's Run <53133058+s4turns@users.noreply.github.com>
2019-08-09 22:33:18 -0500

    command handler

--- packbot.js
+++ packbot.js
@@ -176,6 +176,35 @@ client.on('message', message => {
 			console.error(err);
 			message.channel.send('there was an error trying to prune messages in this channel!');
 		});
+    client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 });
 
