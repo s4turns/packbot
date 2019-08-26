bb120705139a5093e2982029cdb30e5587f26888
xDDFF <something.very.random.here@gmail.com>
2019-08-26 14:57:05 +0000

    added prune command

---
+++ commands/prune.js
@@ -0,0 +1,14 @@
+exports.run = (client, message, args) => {
+ const amount = parseInt(args[0]) + 1;
+ 
+ if(isNaN(amount)) {
+  return message.reply('that doesn\'t seem to be a valid number.');
+ } else if (amount <= 1 || amount > 100) {
+  return message.reply('You need to input a number between 1 and 99.');
+ }
+
+ message.channel.bulkDelete(amount, true).catch(err => {
+  console.error(err);
+  message.channel.send('There was an error trying to prune messages in this channel!'); 
+ });
+} 
