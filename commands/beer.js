d721f6043da7fb7b14e4589c604a21fcae2cd53f
xDDFF <something.very.random.here@gmail.com>
2019-08-26 21:20:39 +0000

    added taggedUser functionality to beer

--- commands/beer.js
+++ commands/beer.js
@@ -1,3 +1,7 @@
 exports.run = (client, message, args) => {
- message.reply(`Here! take this! an ice cold beer! is a Warsteiner! Prost!`);
+	let user = args;
+	if (message.mentions.users.size) {
+		return message.channel.send(`Here! ${user} an ice cold beer! is a WarSteiner! Prost!`);
+	}
+	message.reply(`Here! take this! an ice cold beer! is a Warsteiner! Prost!`);
 }
