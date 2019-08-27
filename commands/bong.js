4bc0fba4c6d3cc4d93cf63bff18c6f194df03b5c
xDDFF <something.very.random.here@gmail.com>
2019-08-27 17:37:18 +0000

    fixed bong argument parsing

--- commands/bong.js
+++ commands/bong.js
@@ -3,5 +3,10 @@ exports.run = (client, message, args) => {
  if (message.mentions.users.size) {
 	return message.channel.send(`${message.author.username} passed the bong to ${user}. Lets get baked bitch!`);
  }
- message.channel.send(`passed the bong to ${message.author.username}. Lets get baked bitch!`);                                                  
+ if (args.length  === 0) {
+ return message.channel.send(`passed the bong to ${message.author.username}. Lets get baked bitch!`);                                                  
+ }
+ if (!message.mentions.users.size) {
+  return message.channel.send(`FUCK OFF! that user is not a discord user DAGGOT!`);
+ }
 }
