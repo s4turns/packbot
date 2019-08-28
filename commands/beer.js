f7fc2a1d43d6cf61c0bb960c9cab03b57ca1f8e3
xDDFF <something.very.random.here@gmail.com>
2019-08-28 01:25:08 +0000

    hot patch for beer and bong

--- commands/beer.js
+++ commands/beer.js
@@ -1,7 +1,12 @@
 exports.run = (client, message, args) => {
 	let user = args;
+	if (args.length === 0) {
+		message.reply(`Here! take this! an ice cold beer! is a Warsteiner! Prost!`);
+	}
+	if (!message.mentions.users.size && args.length > 0) {
+		return message.channel.send(`FUCK OFF! that user is not a discord user WAGGOT!`);
+	}
 	if (message.mentions.users.size) {
 		return message.channel.send(`Here! ${user} an ice cold beer! is a WarSteiner! Prost!`);
 	}
-	message.reply(`Here! take this! an ice cold beer! is a Warsteiner! Prost!`);
 }
--- commands/bong.js
+++ commands/bong.js
@@ -1,12 +1,12 @@
 exports.run = (client, message, args) => {
- let user = args[0];                                                                                                                                    
- if (message.mentions.users.size) {
-	return message.channel.send(`${message.author.username} passed the bong to ${user}. Lets get baked bitch!`);
+ let taggedUser = args[0];                                                                  
+ if (args.length === 0) {
+ 	return message.channel.send(`passed the bong to ${message.author.username}. Lets get baked bitch!`);                                                  
  }
- if (args.length  === 0) {
- return message.channel.send(`passed the bong to ${message.author.username}. Lets get baked bitch!`);                                                  
- }
- if (!message.mentions.users.size) {
+ if (!message.mentions.users.size && args.length > 0) {
   return message.channel.send(`FUCK OFF! that user is not a discord user DAGGOT!`);
+ }
+ if (message.mentions.users.size) {
+	return message.channel.send(`passed the bong to ${taggedUser}. Lets get baked bitch!`);
  }
 }
