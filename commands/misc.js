a1287a44ede0454291c75d3e5ea7ee0a0b409e70
xDDFF <something.very.random.here@gmail.com>
2019-08-28 00:11:07 +0000

    fixed pack being triggered from irc

--- commands/misc.js
+++ commands/misc.js
@@ -1,6 +1,4 @@
 exports.run = (client, message, args) => {
-if (	message.content === '!pray') {
-	message.reply('In nomine Patris, et Filii, et Spiritus Sancti. Amen!')
-}
+	console.log(message.author.member)
 }
 
