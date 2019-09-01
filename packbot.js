c5a64990aea3e4b8a3d82e7fdd3f04bd54d5ed62
xDDFF <something.very.random.here@gmail.com>
2019-09-01 00:25:48 +0000

    added taxes to packbot

--- packbot.js
+++ packbot.js
@@ -37,10 +37,11 @@ client.on('message', message => {
  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
  const command = args.shift().toLowerCase();
  if (command === 'pray') {message.reply('In nomine Patris, et Filii, et Spiritus Sancti. Amen!')}
- if (command === 'interdome') {message.reply('interdome? that nigga in #201337, he is so fucking 80s, he is listening to gg allen!!')}
- if (command === 'applegroo') {message.reply('He is the best builder there is, his knowledge of physics empowers his building skills.')}
- if (command === 'edibles') {message.reply(`Enjoy some of these edibles`, { file: "https://media.giphy.com/media/GJ6SBog5lpi7u/giphy.gif" })}
- if (command === 'joint') {message.channel.send(`Lights ${message.author.username} joint! smoke up broheim!`)}
+ if (command === 'interdome') {message.reply('interdome? that nigga in #201337, he is so fucking 80s, he is listening to gg allen!!');}
+ if (command === 'applegroo') {message.reply('He is the best builder there is, his knowledge of physics empowers his building skills.');}
+ if (command === 'edibles') {message.reply(`Enjoy some of these edibles`, {file: "https://media.giphy.com/media/GJ6SBog5lpi7u/giphy.gif" });}
+ if (command === 'joint') {message.channel.send(`Lights ${message.author.username} joint! smoke up broheim!`);}
+ if (command === 'tax') {message.channel.send(`taxes? i think you better look at this`, {file: "http://i.imgur.com/mxrbcBX.jpg"});}
 });
 
 client.login(config.token);
