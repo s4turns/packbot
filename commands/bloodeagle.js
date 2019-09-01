7f8bdb2d5fd4e6200efc3cad457187f2ca2f614a
xDDFF <something.very.random.here@gmail.com>
2019-09-01 00:46:09 +0000

    addeds bloodeagle with timers for more suspence

---
+++ commands/bloodeagle.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, args) => {
+	let taggedUser = args[0];  
+	if (!message.mentions.users.size) {
+		message.reply("USAGE: !bloodeagle <username>");
+		return;
+	}
+	if (message.mentions.users.size) {
+		message.channel.send(`np: Wardrune -Runaljod`);
+		setTimeout(() => {message.channel.send(`begins to cut ${taggedUser}`);}, 4500);
+		setTimeout(() => {message.channel.send(`${message.author} watches as ${taggedUser} makes no sound`);}, 6500);
+		setTimeout(() => {message.channel.send(`begins to open ${taggedUser}`);}, 8500);
+		setTimeout(() => {message.channel.send(`tears out the lungs of ${taggedUser} and places them on their shoulders`);}, 10500);
+		setTimeout(() => {message.channel.send(`wacthes as everyone in ${message.channel} looks in horror!`);}, 12500);
+		setTimeout(() => {message.channel.send(`you will now go to valhall my friend....`);}, 14500);
+		return;
+	}
+}
