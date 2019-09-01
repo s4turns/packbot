28f7df49b014590013ddf06d2e1ee075d59b90bf
xDDFF <something.very.random.here@gmail.com>
2019-09-01 00:15:26 +0000

    added wrists, the command only works on one-self no args

---
+++ commands/wrists.js
@@ -0,0 +1,6 @@
+exports.run = (client, message, args) => {
+	let taggedUser = args[0];
+	message.channel.send(`WATCHES AS ${message.author} SLASHES IT'S OWN WRISTS!!`);
+	message.channel.send(`i guess being a faggot and picking extra shifts at the dick factory was to much!!`);
+	return;
+}
