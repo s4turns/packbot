d794fa8a298a66d288b03c7c2921211fbeda0641
xDDFF <something.very.random.here@gmail.com>
2019-09-01 01:05:11 +0000

    fixed another typo in bloodeagle

--- commands/bloodeagle.js
+++ commands/bloodeagle.js
@@ -10,7 +10,7 @@ exports.run = (client, message, args) => {
 		setTimeout(() => {message.channel.send(`${message.author} watches as ${taggedUser} makes no sound`);}, 6500);
 		setTimeout(() => {message.channel.send(`begins to open ${taggedUser}`);}, 8500);
 		setTimeout(() => {message.channel.send(`tears out the lungs of ${taggedUser} and places them on their shoulders`);}, 10500);
-		setTimeout(() => {message.channel.send(`wacthes as everyone in ${message.channel} looks in horror!`);}, 12500);
+		setTimeout(() => {message.channel.send(`wacthes as everyone in ${message.guild} looks in horror!`);}, 12500);
 		setTimeout(() => {message.channel.send(`you will now go to valhall my friend....`);}, 14500);
 		return;
 	}
