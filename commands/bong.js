1500b8095702d88760326e4c4cccbf8ac18863fe
xDDFF <something.very.random.here@gmail.com>
2019-09-07 20:34:35 +0000

    minor cosmetic fixes for bong

--- commands/bong.js
+++ commands/bong.js
@@ -1,12 +1,12 @@
 exports.run = (client, message, args) => {
  let taggedUser = args[0];
  if (args.length === 0) {
- 	return message.channel.send(`Passed the bong to ${message.author} \n Lets get baked bitch!`);
+ 	return message.channel.send(`Passed the bong to ${message.author}.\nLets get baked bitch!`);
  }
  if (!message.mentions.users.size && args.length > 0) {
   return message.channel.send(`FUCK OFF! that user is not a discord user DAGGOT!`);
  }
  if (message.mentions.users.size) {
-	return message.channel.send(`Passed the bong to ${taggedUser}. Lets get baked bitch!`);
+	return message.channel.send(`Passed the bong to ${taggedUser}.\nLets get baked bitch!`);
  }
 }
