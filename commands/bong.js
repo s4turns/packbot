2d01ec515427db4609adf4301384f073470adefe
xDDFF <something.very.random.here@gmail.com>
2019-09-13 18:08:45 +0000

    updated bong on packbot

--- commands/bong.js
+++ commands/bong.js
@@ -1,9 +1,9 @@
 exports.run = (client, message, args) => {
  let taggedUser = args[0];
  
-// if (!message.mentions.users.size && args.length > 0) {
-//  return message.channel.send(`FUCK OFF! that user is not a discord user DAGGOT!`);
-// }
+ if (!message.mentions.users.size && args.length > 0) {
+  return message.channel.send(`FUCK OFF! that user is not a discord user DAGGOT!`);
+ }
  if (args.length === 0) {
  	return message.channel.send(`Passed the bong to ${message.author}\nLets get baked bitch!`);
  }
