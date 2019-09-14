e80a5dea3084432e5b173fbf54d8c3ed71b22da0
xDDFF <something.very.random.here@gmail.com>
2019-09-14 04:46:24 +0000

    fixed minor bug in bong

--- commands/bong.js
+++ commands/bong.js
@@ -3,9 +3,9 @@ exports.run = (client, message, args) => {
  let role = "323550435157999616";
  let roleMembers = message.guild.roles.get(role).members.map(m=>m.user.tag);
  
- if (!message.mentions.users.size && args.length > 0) {
-  return message.channel.send(`FUCK OFF! that user is not a discord user DAGGOT!`);
- }
+// if (!message.mentions.users.size && args.length > 0) {
+//  return message.channel.send(`FUCK OFF! that user is not a discord user DAGGOT!`);
+// }
  if (args.length === 0) {
  	return message.channel.send(`Passed the bong to ${message.author}\nLets get baked bitch!`);
  }
