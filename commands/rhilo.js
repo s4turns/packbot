93e22b4f67b9bac419b6a7d478b00b89b9077678
s4turns <53133058+s4turns@users.noreply.github.com>
2020-11-06 16:21:35 -0600

    Update rhilo.js

--- commands/rhilo.js
+++ commands/rhilo.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-	message.channel.send(`MAGA is my favorite vision, I love Donald Trump as my emperor {file: 'https://i.imgur.com/AVCLPU6.jpg' }`);
+	message.channel.send("MAGA is my favorite vision, I love Donald Trump as my emperor", {files: ["https://i.imgur.com/AVCLPU6.jpg"]});
 }
