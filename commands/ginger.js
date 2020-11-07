0bb06f6a76388648879d126d7d94a3fb08117c2e
s4turns <53133058+s4turns@users.noreply.github.com>
2020-11-07 11:18:15 -0600

    Update ginger.js

--- commands/ginger.js
+++ commands/ginger.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-	message.channel.send("Oh look its mandy", {files: ["https://i.imgur.com/6bQMsFm.png"]});
+	message.channel.send("Oh look its mandy", {files: ["https://i.ibb.co/6mD19pz/image.png"]});
 }
