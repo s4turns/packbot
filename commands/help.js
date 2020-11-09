cdf22c7555fd6d844c1c47e4d88a75d23b1d6218
s4turns <53133058+s4turns@users.noreply.github.com>
2020-11-08 21:37:16 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
- message.reply("`My current commands are: !pack !bong !pray !beer !rounds !applegroo !interdome !ping !blah !ud !bøtte !prune !kick !server !avatar !rape !wrists !hacker !stabs !ddos !bloodeagle !dff !edibles !speak`");
+ message.reply("`My current commands are: !pack !bong !pray !beer !rounds !applegroo !interdome !ping !blah !ud !bøtte !prune !kick !server !avatar !rape !wrists !hacker !stabs !ddos !bloodeagle !dff !edibles !roll !rhilo !ginger`");
 }
