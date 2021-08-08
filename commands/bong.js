213ca70addad228243a0a314562fd3d82ed49e8c
s4turns <53133058+s4turns@users.noreply.github.com>
2021-08-08 17:09:57 -0500

    Update bong.js

--- commands/bong.js
+++ commands/bong.js
@@ -4,17 +4,17 @@ exports.run = (client, message, args) => {
 // let roleMembers = message.guild.roles.get(role).members.map(m=>m.user.tag);
  
  if (!message.mentions.users.size && args.length > 0 && !message.mentions.everyone) {
-  return message.channel.send(`FUCK OFF! that user is not a discord user DAGGOT!`);
+  return message.channel.send(`FUCK OFF! That user is not a discord user DAGGOT!`);
  }
  if (args.length === 0) {
- 	return message.channel.send(`Passed the bong to ${message.author}\nLets get baked bitch!`);
+ 	return message.channel.send(`Passed the bong to ${message.author}\nLets get baked bitch! You better not be parking on that grass!`);
  }
  if (message.mentions.users.size) {
-	return message.channel.send(`Passed the bong to ${taggedUser}.\nLets get baked bitch!`);
+	return message.channel.send(`Passed the bong to ${taggedUser}.\nLets get baked bitch! You better not be parking on that grass!`);
  }
  if (message.mentions.everyone) {
   roleMembers.forEach(user => {
-   message.channel.send(`Passes the bong to ${user}\nLets get baked bitch!`);
+   message.channel.send(`Passes the bong to ${user}\nLets get baked bitch! You better not be parking on that grass!`);
   });
  }
 }
