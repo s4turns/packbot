57c77ea6dd2b1af1adbaca492589226dc777c6eb
s4turns <53133058+s4turns@users.noreply.github.com>
2021-08-17 09:28:36 -0500

    Update packbot.js

--- packbot.js
+++ packbot.js
@@ -37,15 +37,15 @@ client.on('message', message => {
  if (message.author.bot) return;
  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
  const command = args.shift().toLowerCase();
- if (command === 'pray') {message.reply('in nomine Patris, et filii, et spiritus sancti. amen!')}
+ if (command === 'pray') {message.reply('In nomine Patris, et filii, et spiritus sancti. amen!')}
  if (command === 'interdome') {message.reply('interdome? that nigga in #201337, he is so fucking 80s, he is listening to gg allen!!');}
- if (command === 'applegroo') {message.reply('he is the best builder there is, his knowledge of physics empowers his building skills.');}
- if (command === 'edibles') {message.reply(`enjoy some of these edibles`, {file: "https://media.giphy.com/media/GJ6SBog5lpi7u/giphy.gif" });}
- if (command === 'joint') {message.channel.send(`lights ${message.author.username} joint! smoke up broheim!`);}
- if (command === 'tax') {message.channel.send(`taxes? i think you better look at this`, {file: "http://i.imgur.com/mxrbcBX.jpg"});}
+ if (command === 'applegroo') {message.reply('He is the best builder there is, his knowledge of physics empowers his building skills.');}
+ if (command === 'edibles') {message.reply(`Enjoy some of these edibles`, {file: "https://media.giphy.com/media/GJ6SBog5lpi7u/giphy.gif" });}
+ if (command === 'joint') {message.channel.send(`Lights ${message.author.username} joint! smoke up broheim!`);}
+ if (command === 'tax') {message.channel.send(`Taxes? I think you better look at this`, {file: "http://i.imgur.com/mxrbcBX.jpg"});}
  if (command === 'dff') {message.channel.send(`DFF: Dobby the Faggot Ferret!!!1 <3 `, {file: "https://i.imgur.com/7nac3rM.jpg"});}
- if (command === 'bøtte') {message.channel.send(`hre enjoy this bøtte!`, {file: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Buckie.JPG/800px-Buckie.JPG"});}
- if (command === 'roll') {message.channel.send(`fuck yeah!! ${message.author.username} is rolling up that joint, be ready for the great passing!`, {file: "https://i.redd.it/klvo4ye5v7sz.jpg"});}
+ if (command === 'bøtte') {message.channel.send(`Here enjoy this bøtte!`, {file: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Buckie.JPG/800px-Buckie.JPG"});}
+ if (command === 'roll') {message.channel.send(`Fuck yeah!! ${message.author.username} is rolling up that joint, be ready for the great passing!`, {file: "https://i.redd.it/klvo4ye5v7sz.jpg"});}
  if (command === 'vape') {message.channel.send(`${message.author.username} is preparing to heat up some good shit! Pack that vaper!`, {file: "https://weedmaps.com/news/wp-content/uploads/2021/01/4943_Big-Bear_Dry-Herb-Vape_020-2-2.jpg"});}
  if (command === 'float') {message.channel.send(`${message.author.username} is floating away high as FUCK!`, {file: "https://raw.githubusercontent.com/s4turns/PackBot/master/files/images/float.gif"});}
 });
